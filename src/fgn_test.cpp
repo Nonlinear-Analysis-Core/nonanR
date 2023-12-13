2ce961967e0693f225eea3fcacd2eb9bd7c2811b
JSommerfeld36 <51831251+JSommerfeld36@users.noreply.github.com>
2023-12-13 10:58:17 -0600

    Added comments to where difference to R occur

--- src/fgn_test.cpp
+++ src/fgn_test.cpp
@@ -13,6 +13,7 @@ using namespace Rcpp;
    //arma::vec ans = arma::randn(n); // create a fake output just for testing
    
    // Generate Sequence
+   std::srand(123456);
    arma::vec z = arma::randn(2 * n); // Generate normally distributed random numbers
    arma::vec zr = z.subvec(0, n - 1); // minus one because zero based - first half of `z`
    arma::vec zi = z.subvec(n, 2 * n - 1); // plus one because zero based - second half of `z`
@@ -22,63 +23,70 @@ using namespace Rcpp;
    zi[zi.size() - 1] = 0; // Make the last number of `zi` equal 0.
    zr[zr.size() - 1] = zr[zr.size() - 1] * std::sqrt(2); // take the last element and multiply that by the sqrt of 2 
    
+   // Matches perfectly with R up to here.
+   
+   
    // Take the zr vector, reverse it, take the first 100 points of it, and the first 98 of the reversed vector and join them together.
    arma::vec zr_subvec = zr.subvec(0, n - 1); // first part of zr_concat    
    arma::vec flipped_zr = arma::flipud(zr); // second part of zr_concat
-   flipped_zr = flipped_zr.subvec(0, flipped_zr.n_elem - 3); // Remove the last elements - second part of zr_concat
+   flipped_zr = flipped_zr.subvec(1, flipped_zr.n_elem - 2); // Remove the last elements - second part of zr_concat
    arma::vec zr_concat = arma::join_cols(zr_subvec, flipped_zr); // join the two vectors
-   
-   
+ 
+
    // Do the same thing here
    arma::vec zi_subvec = zi.subvec(0, n - 1); // first part of zi_concat    
    arma::vec flipped_zic = arma::flipud(zic); // second part of zi_concat
-   flipped_zic = flipped_zic.subvec(0, flipped_zic.n_elem - 3); // Remove the last elements - second part of zi_concat
+   flipped_zic = flipped_zic.subvec(1, flipped_zic.n_elem - 2); // Remove the last elements - second part of zi_concat
    arma::vec zi_concat = arma::join_cols(zi_subvec, flipped_zic); // join the two vectors
    
    arma::cx_vec z_complex = arma::cx_vec(zr_concat, zi_concat); // Create a vector of real and imaginary numbers
-   
+    
    // Calculate parameters for FGN
-   arma::vec k = arma::regspace(0, n - 1); // 1000 points long
+   arma::vec k = arma::regspace(0, n - 1); // 0 to n
    arma::vec gammak = (arma::pow(abs(k - 1), 2 * H) - 2 * arma::pow(abs(k), 2 * H) + arma::pow(abs(k + 1), 2 * H)) / 2; // This seems to check out
-   
-   
+    
+    
    // define the elements to join. These should create a pyramid with 999 being the point in the middle of the vector.
    arma::uvec A = arma::regspace<arma::uvec>(0, n - 1);
    arma::uvec B = arma::regspace<arma::uvec>(n - 2, 1);
    arma::uvec ind = arma::join_cols(A, B); // join the columns A and B to create one long vector
-   
+    
    arma::vec shifted_gammak = gammak.elem(ind); // Takes the elements of gammak at index `ind`
+    
+   //arma::cx_colvec complex_gammak = arma::conv_to<arma::cx_vec>::from(shifted_gammak); // convert to a complex vector
+   arma::cx_mat gkFGN0 = arma::fft(shifted_gammak); /// --->> the first difference between R and C++ is here
+    
+   arma::vec gksqrt = arma::real(gkFGN0); // differences here only at e-16; quite small
    
-   arma::cx_colvec complex_gammak = arma::conv_to<arma::cx_vec>::from(shifted_gammak); // convert to a complex vector
-   arma::cx_mat gkFGN0 = arma::ifft(complex_gammak);
-   
-   arma::vec gksqrt = arma::real(gkFGN0);
-   
-   arma::vec temp;
+   arma::cx_vec temp1;
    
    // Check if all elements in gksqrt are positive
    if (all(gksqrt > 0)) {
      gksqrt = arma::sqrt(gksqrt);
      z_complex = z_complex % arma::conv_to<arma::cx_vec>::from(gksqrt);
      
-     z_complex = arma::ifft(z_complex);
+     z_complex = arma::fft(z_complex); // quite different at this point ------------------
      
-     double one = std::pow(n - 1, -0.5);
-     double two = one * 0.5;
+     //     z1 = 0.5*(n-1)**(-0.5)*z
+     arma::cx_vec temp = std::pow(0.5 * (n - 1), -0.5) * z_complex;
+     
+     
+     //double one = std::pow(n - 1, -0.5);
+     //double two = one * 0.5;
      // Everything seems to be fine until here
      
-     z_complex = z_complex * two;
-     arma::vec temp = arma::real(z_complex);
-     ans = temp.head(n);
-   } else {
+     //z_complex = z_complex * two;
+     arma::vec temp1 = arma::real(temp);
+     //ans = temp1.head(n);
+   //} else {
      //    gksqrt.zeros();
      //    stop("Re(gk)-vector not positive");
    }
    //
    // Standardize the result
-   ans = std * ans + mean;
+   //ans = std * ans + mean;
    
-   return(temp); // Return the generated FGN
+   return(temp1); // Return the generated FGN
  }
  
  

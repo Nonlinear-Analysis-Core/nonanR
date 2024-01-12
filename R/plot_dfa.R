25d46fe18dc3e1d8bdcc4a28a80d43af14098193
Joel Sommerfeld <joel.sommerfeld36@gmail.com>
2024-01-12 15:40:32 -0600

    Added plotting function for dfa results

---
+++ R/plot_dfa.R
@@ -0,0 +1,45 @@
+#' Detrended Fluctuation Analysis Plot
+#' 
+#' Plot method for monofractal detrended fluctuation analysis
+#' 
+#' 
+#' 
+#' @param x The list object returned \code{dfa} in this package.
+#' 
+#' @examples
+#' 
+#' # Create a time series and perform DFA
+#' x = rnorm(1000)
+#' order = 1
+#' verbose = 1
+#' scales = c(16,32,64,128,256,512)
+#' scale_ratio = 2
+#' 
+#' dfa_out = dfa(x, order, verbose, scales, scale_ratio)
+#' 
+#' # Plot dfa_out
+#' plot_dfa(dfa_out)
+#' 
+#' @export
+#' 
+plot_dfa = function(x){
+  library(ggplot2)
+  
+  dat = do.call(cbind.data.frame, dfa_out)
+  m = lm(dfa_out$log_rms ~ dfa_out$log_scales)
+  r2 = format(summary(m)$r.square,4, digits = 3)
+  plot.text = paste0("Alpha = ", round(dat$alpha[1], 3), "\n", "R^2 = ", (r2))
+  x_pos = max(dat$log_scales)-1
+  y_pos = dat$log_rms[2]:dat$log_rms[1] 
+  
+  ggplot(dat, aes(x = log_scales, y = log_rms)) + 
+    geom_smooth(method=lm, se = FALSE, color="red") + 
+    geom_point(size = 3) + 
+    annotate(geom = "label", x = x_pos, y = y_pos, label = plot.text, label.size = NA, fill = "lightgray") +
+    labs(title = "Detrended Fluctuation Analysis", 
+         x = "Scale (log)", 
+         y = "Fluctuation (log)") +
+    NONANr::theme_nonan()
+}
+
+

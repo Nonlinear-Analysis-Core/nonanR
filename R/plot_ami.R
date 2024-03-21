a3819426ed5ccd8c29cba40b96865eae5b8b2c1b
JSommerfeld36 <51831251+JSommerfeld36@users.noreply.github.com>
2024-03-21 09:13:04 -0500

    Editing label aesthetics

--- R/plot_ami.R
+++ R/plot_ami.R
@@ -31,7 +31,12 @@ plot_ami = function(x){
   ggplot(dat, aes(x = dat[,1], y = dat[,2])) + 
     geom_line(linewidth = 1) +
     geom_point(size = 2) +
-    geom_point(data = min_tau, aes(x = min_tau[,1], y = min_tau[,2]), size = 2, color = "#C8102E") + 
+    geom_point(data = min_tau, aes(x = min_tau[,1], y = min_tau[,2]), size = 4, color = "#C8102E") + 
+    geom_label(label = paste0("Tau = ", round(min_tau[,2], 3),  "\n", "Lag = ", min_tau[,1]), 
+                              x = min_tau[,1] + 5, 
+                              y = min_tau[,2] *5, 
+               label.size = NA, 
+               fill = "lightgray") +
     labs(title = "AMI vs Lag", 
          x = "Lag", 
          y = "AMI") +

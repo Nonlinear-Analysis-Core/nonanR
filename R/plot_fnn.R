9cef25de18447baa7e23705435823aa7d5291b21
JSommerfeld36 <51831251+JSommerfeld36@users.noreply.github.com>
2024-03-22 14:40:30 -0500

    adding fnn functions to package

---
+++ R/plot_fnn.R
@@ -0,0 +1,45 @@
+#' False Nearest Neighbors Plot
+#' 
+#' Plot method for false nearest neighbors.
+#' 
+#' @param x The list object returned \code{fnn} in this package.
+#' 
+#' @examples
+#' 
+#' # Create a time series and perform FNN algorithm
+#' x = fgn_sim(n = 1000, H = 0.9)
+#' 
+#' tau = ami_out$tau[1,1]
+#' mmax = 12
+#' rtol = 15
+#' atol = 2
+#' L = 1
+#' 
+#' fnn_out = fnn(x = x, tau = tau, mmax = mmax, rtol = rtol, atol = atol)
+#' 
+#' # Plot fnn_out
+#' plot_fnn(fnn_out)
+#' 
+#' @export
+plot_fnn = function(x){
+  
+  dat = as.data.frame(x[1]) # dE data frame
+  
+  #emb_dim = as.numeric(x[2]) # embedding dimension
+  emb_dim = cbind.data.frame(as.numeric(x[2]), dat[as.numeric(x[2]),])
+  
+  ggplot(dat, aes(x = 1:nrow(dat), y = dat[,1])) + 
+    geom_line(linewidth = 1) +
+    geom_point(size = 2) +
+    geom_point(data = emb_dim, aes(x = emb_dim[1,1], y = emb_dim[1,2]), size = 4, color = "#C8102E") + 
+    scale_x_continuous(breaks = scales::pretty_breaks(n = nrow(dat) + 1)) +
+    geom_label(label = paste0("dim = ", emb_dim[1,1],  "\n", "%FNN = ", round(emb_dim[1,2], 3)), 
+               x = nrow(dat) - 1, 
+               y = emb_dim[1,2] *1.3, 
+               label.size = NA, 
+               fill = "lightgray") +
+    labs(title = "False Nearest Neighbors", 
+         x = "Lag", 
+         y = "% False Neighbors") +
+    theme_nonan()
+}
---
+++ src/fnn.cpp
@@ -0,0 +1,122 @@
+#include <RcppArmadillo.h>
+using namespace Rcpp;
+using namespace arma;
+// [[Rcpp::depends(RcppArmadillo)]]
+
+double sdp(arma::vec x); // Function for calculating the population standard deviation
+arma::mat psr(arma::vec x, int m, int tao, int npoints); // Phase space reconstruction
+  
+//' False Nearest Neighbor
+//'
+//' Calculate the average mutual information of a time series.
+//'
+//' @param x - a single column time series
+//' @param tau - the first local minimum from the data. This is a value returned from the \code{ami} function
+//' @param mmax - The maximum embedding dimension. Common practice is to set this to 12.
+//' @param rtol - The near tolerance. Common practice is to set this to 15. 
+//' @param atol - The far tolerance. Used to accommodate for the fact that all neighbors may be far away to begin with. Common practice is to set this to 2.
+//' @returns The output of the algorithm is a list that includes:
+//' \itemize{
+//'  \item \code{dE} A column vector of the percentages of false neighbors at the embedding dimensions up to \code{mmax}
+//'  \item \code{dim} An integer reflecting the time delay at which there is the lowest number of false neighbors
+//' } 
+//' @import Rcpp
+//' @export
+//'
+//' @details AMI is part of the phase space reconstruction step that is needed for some nonlinear analysis methods.
+//' 
+//' 
+//' @examples
+//'
+//' x = rnorm(1000)
+//' tau = ami_out$tau[1,1]
+//' mmax = 12
+//' rtol = 15
+//' atol = 2
+//' 
+//' fnn_out = fnn(x = x, tau = tau, mmax = mmax, rtol = rtol, atol = atol)
+//'
+//'
+// [[Rcpp::export]]
+List fnn(arma::vec x, int tau, int mmax, double rtol, double atol){
+  
+  double Ra = sdp(x);
+  arma::vec fn(mmax+1);
+  fn.zeros();
+  double D = 0.0;
+  double R = 0.0;
+  
+  for (int m = 1; m < mmax + 1; ++m){
+    // perform phase space reconstruction
+    int M = x.n_elem - m*tau;
+    arma::mat Y = psr(x,m,tau,M);
+    for (int n = 0; n < M; ++n){
+      
+      arma::colvec ones(M);
+      ones.ones();
+      // create a matrix, y0(M,m), where each row is equal to 
+      // row n of of the psr, Y
+      arma::mat y0 = ones*Y.row(n);
+      
+      // substract y0 from Y to obtain the distance of row n from 
+      // all other rows
+      arma::vec distance = arma::sqrt(arma::sum(arma::pow(Y-y0,2),1));
+      
+      //TODO: MAYBE USE A FIND FUNCTION INSTEAD OF SORT
+      //      NOTE: I TRIED SOMETHING LIKE THIS IN THE DEV VERSION BUT
+      //      IT WAS SLOWER THAN THIS ONE.
+      // arma::vec neardis = sort(distance);
+      arma::uvec nearpos = sort_index(distance);
+      
+      // only examine the rth nearest neighbor where r = 1 (i.e. Kennel et al.,1992)
+      unsigned int ix1 = n + (m)*tau;
+      unsigned int ix2 = nearpos(1) + (m)*tau;
+      D = std::abs(x(ix1) - x(ix2));
+      R = std::sqrt(std::pow(D,2) + std::pow(distance(nearpos(1)),2));
+      
+      // comapre to tolerances to determine if the a false neighbor
+      if (D/distance(nearpos(1)) > rtol || R/Ra > atol){
+        
+        fn(m) += 1;
+      }
+    }
+  }
+  
+  // discard first element in fn because it is just a zero
+  arma::vec result = fn.subvec(1,mmax);
+  
+  
+  arma::vec dE = (result/result(0))*100;
+  int dim = dE.index_min() + 1; // add one because it is zero based
+
+  return List::create(Named("dE") = dE, Named("dim") = dim);
+}
+
+
+// population standard deviation
+double sdp(arma::vec x){
+  
+  return std::sqrt(arma::accu(arma::pow(x-arma::mean(x),2))/x.n_elem);
+}
+
+
+// do phase space reconstruction
+arma::mat psr(arma::vec x, int m, int tau, int npoints){
+  // int N = x.n_elem;
+  // int M = x.n_elem - m*tao;
+  int M = npoints;
+  arma::mat Y(M,m);
+  Y.zeros();
+  
+  for ( int i = 0; i < m; i++){
+    int start = i*tau;
+    // Rcout << "start is " << start << "\n";
+    int stop = M-1 + (i*tau);
+    // Rcout << "stop is " << stop << "\n";
+    
+    Y.col(i) = x.subvec(start,stop);
+  }
+  
+  return Y;
+  
+}

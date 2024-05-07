f0dbda7f3af2aa03adb408edafae3e20832854bc
JSommerfeld36 <51831251+JSommerfeld36@users.noreply.github.com>
2024-05-07 11:53:04 -0500

    Updated label position for plot

--- R/plot_dfa.R
+++ R/plot_dfa.R
@@ -27,14 +27,16 @@ plot_dfa = function(x){
   dat = do.call(cbind.data.frame, x)
   m = lm(dat[,2] ~ dat[,1])
   r2 = format(summary(m)$r.square,4, digits = 3)
-  plot.text = paste0("Alpha = ", round(dat[,3][1], 3), "\n", "R^2 = ", (r2))
-  x_pos = max(dat[,1])-1
-  y_pos = dat[,2][2]:dat[,2][1] 
+  
+  plot.text = paste0("Alpha = ", round(dat[,3][1], 3), "\n", "R^2 = ", (r2), sep = "")
   
   ggplot(dat, aes(x = dat[,1], y = dat[,2])) + 
     geom_smooth(formula = y ~ x, method = lm, se = FALSE, color="#C8102E") + 
     geom_point(size = 3) + 
-    annotate(geom = "label", x = x_pos, y = y_pos, label = plot.text, label.size = NA, fill = "lightgray") +
+    geom_label(label = plot.text, 
+               x = Inf, y = -Inf, 
+               parse = F, vjust = "bottom", hjust = "inward",
+               label.size = NA, fill = "lightgray") +
     labs(title = "Detrended Fluctuation Analysis", 
          x = "Scale (log)", 
          y = "Fluctuation (log)") +

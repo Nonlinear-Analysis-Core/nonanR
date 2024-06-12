47510bbf38a9c6db2691ed3f9fecadd3ce6690be
JSommerfeld36 <51831251+JSommerfeld36@users.noreply.github.com>
2024-06-12 08:50:47 -0500

    Updating run_gui name and docs

--- R/run_gui.R
+++ R/run_gui.R
@@ -1,21 +1,25 @@
-#' This function allows you to run a shiny app that will do all the things for you
+#' A point-and-click Shiny app to assist in nonlinear analysis
 #' 
+#' @details
+#' While there are no explicit function arguments, the app does require the user to have a data frame in the global environment. 
 #' 
-#' Once the app is running, you can select the analysis type that you want to use from the top tab menu and then select your data frame and the column you want to analyse using the options on the left hand side. 
+#' Once the app is running, you can select the analysis type that you wish to use from the top tab menu and then select 
+#' your data frame and the column you want to analyse using the dropdown menu on the left hand side. 
 #' The analysis methods have suggested inputs for you to use however, it is strongly recommended that you use parameters specific for your analysis. 
-#'
-#'
 #'
 #' @examples
 #' # Create some mock data (or load in your own) to use in the app.
-#' left_dat = data.frame("stride_number" = 1:1000, "stride_time_left" = rnorm(1000, 1.2, 0.03))
-#' right_dat = data.frame("stride_number" = 1:1000, "stride_time_right" = rnorm(1000, 1.3, 0.05))
+#' example_dat = data.frame("Index" = 1:1000, 
+#'                          "amplitude_1" = fgn_sim(n = 1000, H = 0.8, mean = 1.2, std = 0.03), 
+#'                          "amplitude_2" = fgn_sim(n = 1000, H = 0.7, mean = 1.25, std = 0.04))
 #' 
 #' 
-#' \dontrun{runGUI()}
+#' \dontrun{ 
+#' run_gui()
+#' }
 #'
 #' @export
-runGUI <- function() {
+run_gui <- function() {
   appDir <- system.file("NONANapp", "NONANr_app.R", package = "nonanR")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `nonanR`.", call. = FALSE)
